f630a0e4194a06e09d66709107277665eb5314d3
zydou <git@zydou.me>
2023-03-02 16:35:18 +0800

    fix(public_ip): use OpenDNS non-53 port.
    OpenDNS's 53 port is blocked (partially?) in China while non-53 port is not affected.
    
    perf(download): change download url
    
    feat(public_ip): add `q` and `digtcp` method

--- internal/p10k.zsh
+++ internal/p10k.zsh
@@ -1534,10 +1534,20 @@ _p9k_prompt_public_ip_async() {
     case $method in
       dig)
         if (( $+commands[dig] )); then
-          ip="$(dig +tries=1 +short -4 A myip.opendns.com @resolver1.opendns.com 2>/dev/null)"
+          ip="$(dig +short -4 A myip.opendns.com @208.67.220.2 -p 443 2>/dev/null)"
           [[ $ip == ';'* ]] && ip=
           if [[ -z $ip ]]; then
-            ip="$(dig +tries=1 +short -6 AAAA myip.opendns.com @resolver1.opendns.com 2>/dev/null)"
+            ip="$(dig +short A whoami.akamai.net @ns1-1.akamaitech.net 2>/dev/null)"
+            [[ $ip == ';'* ]] && ip=
+          fi
+        fi
+      ;;
+      q)
+        if (( $+commands[q] )); then
+          ip="$(command q -r @208.67.220.2:443 A myip.opendns.com 2>/dev/null)"
+          [[ $ip == ';'* ]] && ip=
+          if [[ -z $ip ]]; then
+            ip="$(command q -r @'[2620:0:ccd::2]':443 AAAA myip.opendns.com 2>/dev/null)"
             [[ $ip == ';'* ]] && ip=
           fi
         fi
@@ -7548,7 +7558,7 @@ _p9k_init_params() {
     esac
   done
   _p9k_declare -F POWERLEVEL9K_PUBLIC_IP_TIMEOUT 300
-  _p9k_declare -a POWERLEVEL9K_PUBLIC_IP_METHODS -- dig curl wget
+  _p9k_declare -a POWERLEVEL9K_PUBLIC_IP_METHODS -- dig4 dig6 q dig curl wget
   _p9k_declare -e POWERLEVEL9K_PUBLIC_IP_NONE ""
   _p9k_declare -s POWERLEVEL9K_PUBLIC_IP_HOST "https://v4.ident.me/"
   _p9k_declare -s POWERLEVEL9K_PUBLIC_IP_VPN_INTERFACE ""
